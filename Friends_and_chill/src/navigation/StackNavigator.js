7a3d6f17a80df478cceeaab6309f2efcacd63bde
itsvaalentine <96605134+itsvaalentine@users.noreply.github.com>
2025-05-21 01:02:25 -0600

    change stack

--- Friends_and_chill/src/navigation/StackNavigator.js
+++ Friends_and_chill/src/navigation/StackNavigator.js
@@ -9,8 +9,8 @@ const Stack = createNativeStackNavigator();
 export default function AppNavigator() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="LoginScreen" component={LoginScreen} />
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="LoginScreen" component={LoginScreen} />
     </Stack.Navigator>
 
   );
